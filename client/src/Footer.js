1fa4e331559fb8b6684f9bab5d2457535ccb460b
akhtarha <akhtar.hassan@hotmail.co.uk>
2017-12-20 18:28:16 +0000

    Added client

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+
+export default () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <span className="text-muted">Booking.io 2017</span>
+            </div>
+        </footer>
+    );
+};
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import App from './App.js';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
